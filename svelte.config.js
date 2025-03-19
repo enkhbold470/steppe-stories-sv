8d8ed8812b9c2f947859c08c5b0002039a4ab759
enkhbold470 <enkhbold470@gmail.com>
2025-03-18 22:30:29 -0700

    Update production script in package.json to use node for building; switch Svelte adapter to @sveltejs/adapter-node for improved server compatibility; add new dependencies for Rollup plugins and enhance configuration options.

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,9 +7,16 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
+	
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			precompress: true,
+			strict: true,
+			env: {
+				PORT: process.env.PORT || 3000
+			}
+		})
 	},
 	alias: {
 		$lib: './src/lib',
